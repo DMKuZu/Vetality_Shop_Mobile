2da7d66dcd517c00822116df62c2c9856d3ff007
JwLappie <john.winston.tabada@gmail.com>
2025-02-17 11:12:31 +0800

    logo is edited

--- app/src/main/java/com/csit284/vetalityshop/LoginActivity.kt
+++ app/src/main/java/com/csit284/vetalityshop/LoginActivity.kt
@@ -2,6 +2,9 @@ package com.csit284.vetalityshop
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +14,21 @@ class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.username)
+        val passwordEditText = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text
+            val password = passwordEditText.text
+
+            if (username.isNullOrEmpty() || password.isNullOrEmpty()) {
+                Toast.makeText(this, "Fields cannot be empty!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
     }
+
+
 }
