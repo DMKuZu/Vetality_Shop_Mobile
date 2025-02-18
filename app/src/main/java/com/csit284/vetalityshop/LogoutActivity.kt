41dcea5288bc2012d60c3dc08927738b50171401
JwLappie <john.winston.tabada@gmail.com>
2025-02-18 10:00:03 +0800

    alertdialog logout

--- app/src/main/java/com/csit284/vetalityshop/LogoutActivity.kt
+++ app/src/main/java/com/csit284/vetalityshop/LogoutActivity.kt
@@ -1,6 +1,8 @@
 package com.csit284.vetalityshop
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,5 +13,21 @@ class LogoutActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logout)
+
+        showLogoutConfirmationDialog()
+    }
+
+    private fun showLogoutConfirmationDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Are you sure you want to logout?")
+        .setPositiveButton("Yes") { dialog, id ->
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        .setNegativeButton("No") { dialog, id ->
+            dialog.dismiss()
+        }
+        builder.create().show()
     }
 }
