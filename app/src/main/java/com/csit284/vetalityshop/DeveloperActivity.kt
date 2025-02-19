4300b05e2e10c0e11367d0d3ce3661b5a6b6dc84
LAB13_USER ON WS11 <L11X13W11@citulab.net>
2025-02-19 14:51:18 +0800

    developer and profile

--- app/src/main/java/com/csit284/vetalityshop/DeveloperActivity.kt
+++ app/src/main/java/com/csit284/vetalityshop/DeveloperActivity.kt
@@ -1,7 +1,9 @@
 package com.csit284.vetalityshop
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,11 @@ class DeveloperActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_developer)
+
+        val btn_close = findViewById<Button>(R.id.btn_close);
+        btn_close.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
