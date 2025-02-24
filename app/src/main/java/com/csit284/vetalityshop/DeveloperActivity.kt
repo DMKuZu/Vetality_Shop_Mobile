fd3e017d9026217605fc920ecb1264ca4e2a7078
JwLappie <john.winston.tabada@gmail.com>
2025-02-24 16:28:13 +0800

    checkpoint for edit profile activity

--- app/src/main/java/com/csit284/vetalityshop/DeveloperActivity.kt
+++ app/src/main/java/com/csit284/vetalityshop/DeveloperActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,5 +15,11 @@ class DeveloperActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_developer)
 
+        val back = findViewById<ImageButton>(R.id.back)
+        var intent: Intent
+
+        back.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/csit284/vetalityshop/EditProfileActivity.kt
+++ app/src/main/java/com/csit284/vetalityshop/EditProfileActivity.kt
@@ -1,20 +1,18 @@
 package com.csit284.vetalityshop
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageButton
 
-class EditProfileActivity : AppCompatActivity() {
+class EditProfileActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_edit_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val back = findViewById<ImageButton>(R.id.back)
+
+        back.setOnClickListener {
+            finish()
         }
     }
 }
