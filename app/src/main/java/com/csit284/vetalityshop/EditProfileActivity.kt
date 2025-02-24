c1b951a523abf4e7e09e6b3fe03075089b77c7e0
JwLappie <john.winston.tabada@gmail.com>
2025-02-24 17:31:36 +0800

    wow username can be edited but is only temporary

--- app/src/main/java/com/csit284/vetalityshop/EditProfileActivity.kt
+++ app/src/main/java/com/csit284/vetalityshop/EditProfileActivity.kt
@@ -1,18 +1,47 @@
 package com.csit284.vetalityshop
 
 import android.app.Activity
+import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class EditProfileActivity : Activity() {
+    private lateinit var editName: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
         val back = findViewById<ImageButton>(R.id.back)
+        val editUsernameButton = findViewById<Button>(R.id.edit_username_btn)
+        editName = findViewById(R.id.edit_username)
 
         back.setOnClickListener {
             finish()
         }
+
+        editUsernameButton.setOnClickListener {
+            val updatedName = editName.text
+
+            if(updatedName.isNullOrEmpty()){
+                Toast.makeText(this, "Input a Username!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("updatedName", updatedName.toString().trim())
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
+
 }
--- app/src/main/java/com/csit284/vetalityshop/ProfileActivity.kt
+++ app/src/main/java/com/csit284/vetalityshop/ProfileActivity.kt
@@ -1,22 +1,32 @@
 package com.csit284.vetalityshop
 
+import android.graphics.drawable.BitmapDrawable
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.provider.MediaStore
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class ProfileActivity : Activity() {
+
+//    private lateinit var defaultProfile: Bitmap
+    private lateinit var defaultName: String
+    private val REQUEST_CODE_EDIT_PROFILE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
         val edit = findViewById<ImageButton>(R.id.edit)
+//        val profilepic = findViewById<ImageView>(R.id.defaultprofile)
+        val username = findViewById<TextView>(R.id.defaultname)
+
         val settings = findViewById<ImageButton>(R.id.settings)
         val logout = findViewById<ImageButton>(R.id.logout)
         val cart = findViewById<ImageButton>(R.id.cart)
@@ -25,6 +35,16 @@ class ProfileActivity : Activity() {
         val list = findViewById<ImageButton>(R.id.list)
         val profile = findViewById<ImageButton>(R.id.profile)
         var intent : Intent
+
+//        defaultProfile = (profilepic.drawable as BitmapDrawable).bitmap
+        defaultName = username.text.toString()
+
+        edit.setOnClickListener {
+            intent = Intent(this, EditProfileActivity::class.java)
+//            intent.putExtra("defaultProfile", defaultProfile)
+            intent.putExtra("defaultName", defaultName)
+            startActivityForResult(intent, REQUEST_CODE_EDIT_PROFILE)
+        }
 
         settings.setOnClickListener{
             intent = Intent(this, SettingsActivity::class.java)
@@ -41,6 +61,20 @@ class ProfileActivity : Activity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_EDIT_PROFILE && resultCode == Activity.RESULT_OK) {
+            data?.extras?.let  {
+//                val updatedProfile = it.getParcelable("updatedProfile",Bitmap::class.java)
+                val updatedName = it.getString("updatedName")
+
+                // Update the ImageView and TextView with the new values
+//                findViewById<ImageView>(R.id.defaultprofile).setImageBitmap(updatedProfile)
+                findViewById<TextView>(R.id.defaultname).text = updatedName
+            }
+        }
+    }
     private fun showLogoutConfirmationDialog(){
         val builder = AlertDialog.Builder(this)
         builder.setMessage("Are you sure you want to logout?")
