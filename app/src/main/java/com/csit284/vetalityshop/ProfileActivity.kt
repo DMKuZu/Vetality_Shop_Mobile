ffb2b8067e20fc08e5b637c907dbbdd8227ad7ba
JwLappie <john.winston.tabada@gmail.com>
2025-02-24 13:11:00 +0800

    constrained profile activity, added edit profile activity

--- app/src/main/java/com/csit284/vetalityshop/ProfileActivity.kt
+++ app/src/main/java/com/csit284/vetalityshop/ProfileActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +15,30 @@ class ProfileActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val edit = findViewById<ImageButton>(R.id.edit)
+        val settings = findViewById<ImageButton>(R.id.settings)
+        val cart = findViewById<ImageButton>(R.id.cart)
+        val home = findViewById<ImageButton>(R.id.home)
+        val favourites = findViewById<ImageButton>(R.id.favourites)
+        val list = findViewById<ImageButton>(R.id.list)
+        val profile = findViewById<ImageButton>(R.id.profile)
+        var intent : Intent
+
+        settings.setOnClickListener{
+            intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        home.setOnClickListener{
+            intent = Intent(this, LandingActivity::class.java)
+            startActivity(intent)
+        }
+
+        profile.setOnClickListener{
+            intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun showLogoutConfirmationDialog(){
